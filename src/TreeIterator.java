39a25d29f1a9b02667a8cc839623e79d638688e7
Aaron <asher897@gmail.com>
2021-08-31 17:03:15 +0200

    Modified the tree iterator to work with a stack and finished the methods.

--- src/TreeIterator.java
+++ src/TreeIterator.java
@@ -1,35 +1,36 @@
+import java.util.*;
+
 public class TreeIterator<T extends Comparable<T>> {
     private BinaryNode<T> current;
-    private BinaryNode<T> left;
-    private BinaryNode<T> right;
-    private BinaryNode<T> previous;
+    private Stack<BinaryNode<T>> iter;
 
-    public TreeIterator(BinaryNode<T> root) {
-        this.current = root;
-        this.left = root.getLeft();
-        this.right = root.getRight();
-        this.previous = null;
+    public TreeIterator(BinarySearchTree<T> tree) {
+        this.current = tree.getRoot();
+        while (!(this.current.getLeft() != null)) {
+            this.iter.add(this.current);
+            this.current = this.current.getLeft();
+        }
     }
 
-    public BinaryNode<T> nextLeft() {
-        BinaryNode<T> temp = new BinaryNode<T>();
-        temp = this.current;
-        this.previous = this.current;
-        this.current = this.left;
-        this.left = this.current.getLeft();
-        this.right = this.current.getRight();
-
+    public BinaryNode<T> next() {
+        BinaryNode<T> temp = this.iter.pop();
+        traverse(temp);
         return temp;
     }
 
-    public BinaryNode<T> nextRight() {
-        BinaryNode<T> temp = new BinaryNode<T>();
-        temp = this.current;
-        this.previous = this.current;
-        this.current = this.left;
-        this.left = this.current.getLeft();
-        this.right = this.current.getRight();
-
-        return temp;
+    public boolean isEnd() {
+        return this.current.isLeaf();
     }
+
+    public boolean hasNext() {
+        return iter.peek() != null;
+    }
+
+    private void traverse(BinaryNode<T> node) {
+        while (!(node.getLeft() != null)) {
+            this.iter.add(node);
+            node = node.getLeft();
+        }
+    }
+
 }
